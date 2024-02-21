b5858e9ca5c5ead68349ecc1caf9afc7abf30489
FL45h-09 <vishal.ravanank@gmail.com>
2024-02-21 09:40:09 +0530

    vishal: coparison, conditional files added

---
+++ vishal/02.09-Comparison.js
@@ -0,0 +1,18 @@
+// Compariosn between the datatypes and their values
+
+// console.log( 2 > 1 );  // true (correct)
+// console.log( 2 == 1 ); // false (wrong)
+// console.log( 2 != 1 ); // true (correct)
+
+
+
+// You can assign the result to the variable 
+
+let result = 5 > 4;
+// console.log( result );
+
+
+
+// other coparisons 
+console.log( null === undefined ); // false
+console.log( null == undefined ); // true
