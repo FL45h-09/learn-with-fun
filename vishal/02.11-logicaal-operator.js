9059fcf37410baf74d8fb504465e1d1b56f6a779
VishalR <vishal.ravanank@gmail.com>
2024-02-22 10:30:34 +0530

    vs config

--- vishal/02.11-logicaal-operator.js
+++ vishal/02.11-logicaal-operator.js
@@ -10,7 +10,7 @@
 
 // && And Operator
 
-// console.log(true && true); // This will give us ture
+console.log(true && true); // This will give us ture
 // console.log(false && true); // This will give us false
 // console.log(true && false); // This will give us false
 // console.log(false && false); // This will give us false
@@ -20,8 +20,8 @@
 
 // syntax
 
-if (!value){
-    run this ConvolverNode.
-}
+// if (!value){
+//     run this ConvolverNode.
+// }
 
-in abve example if the value is not true then it will run the code.``
+// in abve example if the value is not true then it will run the code.
