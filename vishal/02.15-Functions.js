d3d69212926d2cdd9095dbab2d148c032bd59528
FL45h-09 <vishal.ravanank@gmail.com>
2024-02-27 08:48:30 +0530

    vishal: switches and fucntions

---
+++ vishal/02.15-Functions.js
@@ -0,0 +1,38 @@
+// Lets Lern Functions
+
+
+// Syntax
+
+
+// this is how you difene a function
+// function helloWorld(){
+//     console.log("Hello World");
+// }
+
+// // And this is how you call a function to excute the code block in that function.
+// helloWorld();
+
+
+// Local variables
+
+// variables useed inside the function are loca variable to that function and not accessible outside of that function.
+
+// function localVariable(){
+//     var x = 10;
+//     console.log(x);
+// }
+// // console.log(x); // This will give an error "x is not defined"
+// localVariable();
+
+
+// Global Variables
+
+// let names = "Vishal";
+
+// function helloWorld(){
+//     //here names is a global varible because its outside of the scope of this function and surname is scoped to the function itself.
+//     let surname = "Ravanank";
+//     console.log(names + " " + surname);
+// }
+
+// helloWorld();
