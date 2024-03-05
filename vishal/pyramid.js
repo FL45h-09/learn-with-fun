f15cbe823376f2205d6ce0d5b9425a9d1acb0978
VishalR <vishal.ravanank@gmail.com>
2024-03-05 18:37:46 +0530

    Pyramid

--- vishal/pyramid.js
+++ vishal/pyramid.js
@@ -1,7 +1,33 @@
-let i, j, k;
-for(i= 1; i<=5; i++){ 
-    console.log(i)
-    for(j=1; j<=i; j++){
-        console.log(j);
+// let i, j, k;
+// k = 1;
+// for(i= 1; i<=5; i++){ 
+//     for(j=1; j<=i; j++){
+//         // console.log(i);
+//         document.write(k);
+//         ++k;
+//     }
+//     document.write("<br>")
+// }
+
+// 7 spaces
+//    *   
+//   ***  
+//  ***** 
+// *******
+
+
+let i, j, k, l, m;
+l= 1;
+// k = 1;
+for(i = 4; i>=1; i--){
+    for(j = 1; j<= 7; j++){
+        if(i == j){
+            // document.write("*");
+            for(m = 1; m<= l; m ++){
+                document.write("*");
+            }
+        }
     }
+    document.write("<br>");
+    l++;
 }
