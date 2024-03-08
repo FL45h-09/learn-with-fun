42886a37ba6a56db360e1e6bcc732d279e320c4a
VishalR <vishal.ravanank@gmail.com>
2024-03-08 15:23:41 +0530

    task added

--- vishal/pyramid.js
+++ vishal/pyramid.js
@@ -9,25 +9,30 @@
 //     document.write("<br>")
 // }
 
-// 7 spaces
+// Print Star || 4 rows and 7 columns
 //    *   
 //   ***  
 //  ***** 
 // *******
 
 
-let i, j, k, l, m;
-l= 1;
-// k = 1;
-for(i = 4; i>=1; i--){
-    for(j = 1; j<= 7; j++){
-        if(i == j){
-            // document.write("*");
-            for(m = 1; m<= l; m ++){
-                document.write("*");
-            }
-        }
-    }
-    document.write("<br>");
-    l++;
-}
+// let i, j, k, l, m, n, o;
+// l= 1;
+// k = 0;
+// for(i = 4; i>=1; i--){
+//     document.write("<p>");
+//     for(j = 1; j<= 7 - k; j++){
+//         if(j == i){
+//             // document.write("*");
+//             for(m = 1; m<= l; m ++){
+//                 document.write("<span>*</span>");
+//             }
+            
+//         }else{
+//             document.write("<span>&nbsp;</span>");
+//         }
+//     }
+//     l = l+2;
+//     k = k+2;
+//     document.write("</p>");
+// }
