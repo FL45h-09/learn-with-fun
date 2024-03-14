93fb169a0ec65ac5831494560c8016df92bb839c
unknown <kusum4176@gmail.com>
2024-03-14 16:57:42 +0530

    pattern

---
+++ pinki/task/calculator/pattern/05_numberincreasinpyramid.js
@@ -0,0 +1,14 @@
+let n = 5;
+let string = '';
+for (let i = 1; i <= n; i++){
+    for (let j = 1; j < i; j++){
+        string += j;
+    }
+    string += '\n';
+}
+console.log(string);
+
+// 1
+// 12
+// 123
+// 1234
