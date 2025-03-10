458f868b359dec7ace1c31ae0f190fb9f5c59b4b
VishalR <vishal.ravanank@gmail.com>
2025-03-10 18:56:56 +0530

    clearn

--- vishal/02.13-loops.js
+++
@@ -1,8 +0,0 @@
-// Lets learn about the loops in Javascript
-
-// while loop is used when you want to code to run the 1st time and untile the condtion is fullfiled
-let i = 0;
-while (i < 3) { // shows 0, then 1, then 2
-  console.log(i);
-  i++;
-}
